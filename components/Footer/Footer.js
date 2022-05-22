3b20bf83654e95a211ddeefdd822062fd4a6f431
srshashibhushan <Shashi.as05@gmail.com>
2022-05-22 20:43:37 +0530

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
-import { ImMail4 } from "react-icons/Im";
+import { ImMail4 } from "react-icons/im";
 
 import { SocialIcons } from "../Header/HeaderStyles";
 import {
